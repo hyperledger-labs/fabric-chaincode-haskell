c0776192a5b28d15bd53517a68cfc22c9e418e55
Nick Waywood <nwaywood@au1.ibm.com>
2020-02-13 10:27:01 +1100

    Refactored to pass chaincodestub from user land instead of initFn

--- grpc-client/app/Main.hs
+++ grpc-client/app/Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 import Shim
@@ -10,9 +8,16 @@ import Peer.ProposalResponse as Pb
 
 import Debug.Trace
 
-
 main :: IO ()
-main = start initFn
+main = Shim.start chaincodeStub
 
-initFn :: DefaultChaincodeStub -> Pb.Response
-initFn s = trace (getTxId s) initPayload
+chaincodeStub = ChaincodeStub {
+    initFn = initFunc,
+    invokeFn = invokeFunc
+}
+
+initFunc :: DefaultChaincodeStub -> Pb.Response
+initFunc s = trace (getTxId s) successPayload
+
+invokeFunc :: DefaultChaincodeStub -> Pb.Response
+invokeFunc s = trace (getTxId s) successPayload
