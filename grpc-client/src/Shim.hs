dd3ecaa099ec6d3f21ca20cbfcd46609c4d28dc3
Nick Waywood <nwaywood@au1.ibm.com>
2020-02-18 14:30:03 +1100

    Cleaned up code and added more comments

--- grpc-client/src/Shim.hs
+++ grpc-client/src/Shim.hs
@@ -42,28 +42,14 @@ data ChaincodeStub = ChaincodeStub {
     invokeFn :: DefaultChaincodeStub -> IO Pb.Response
 }
 
--- Env to be accessible from anywhere in the application, this means stub functions can get access to env
--- without them needing to be passed from userland main function
--- data Env = Env {
---     send :: StreamSend ChaincodeMessage,
---     recv :: StreamRecv ChaincodeMessage
--- }
---
--- getSend :: Reader a (StreamSend ChaincodeMessage)
--- getSend = pure $ send
-
--- data HandlerType e a = HandlerType {
---     runHandlerType :: Env -> IO (Either e a)
--- }
-
-
 -- TODO: start :: ChaincodeStub a => (a -> Pb.Response) -> IO ()
 start :: ChaincodeStub -> IO ()
 start chaincodeStub = withGRPCClient clientConfig $ grpcRunner chaincodeStub
 
+-- initialise the GRPC communication with the peer
 grpcRunner :: ChaincodeStub -> Client.Client -> IO ()
 grpcRunner chaincodeStub client = do
-        -- contains chaincodeSupportRegister function
+  -- contains chaincodeSupportRegister function
   Pb.ChaincodeSupport {..} <- chaincodeSupportClient client
 
 
@@ -82,15 +68,16 @@ biDiRequestFn chaincodeStub _call _mmap recv send _done = do
     Right _   -> trace "okie dokey" pure ()
   chatWithPeer recv send chaincodeStub
 
+-- main loop listening for messages from the peer
 chatWithPeer recv send chaincodeStub = do
   res <- recv
   case res of
     Left  err            -> error ("OH MY GOD: " ++ show err)
-    -- Right (Just message) -> _
     Right (Just message) -> handler message recv send chaincodeStub
     Right Nothing        -> print "I got no message... wtf"
   chatWithPeer recv send chaincodeStub
 
+-- function to process the different chainccode message types
 handler
   :: ChaincodeMessage
   -> StreamRecv ChaincodeMessage
