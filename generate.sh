782701fe7a500bcaf187960f5a193ab76a4d37e1
Nick Waywood <nwaywood@au1.ibm.com>
2020-02-23 14:28:34 +1100

    Refactord/renamed things for open sourcing
    Signed-off-by: Nick Waywood <nwaywood@au1.ibm.com>

--- generate.sh
+++ generate.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-compile-proto-file --proto ledger/queryresult/kv_query_result.proto --out grpc-client/src --includeDir google-protos --includeDir protos
-compile-proto-file --proto peer/chaincode_shim.proto --out grpc-client/src --includeDir google-protos --includeDir protos
-compile-proto-file --proto peer/chaincode_event.proto --out grpc-client/src --includeDir google-protos --includeDir protos
-compile-proto-file --proto peer/chaincode.proto --out grpc-client/src --includeDir google-protos --includeDir protos
-compile-proto-file --proto peer/proposal_response.proto --out grpc-client/src --includeDir google-protos --includeDir protos
-compile-proto-file --proto peer/proposal.proto --out grpc-client/src --includeDir google-protos --includeDir protos
-compile-proto-file --proto msp/msp_principal.proto --out grpc-client/src --includeDir google-protos --includeDir protos
-compile-proto-file --proto common/policies.proto --out grpc-client/src --includeDir google-protos --includeDir protos
-compile-proto-file --proto google/protobuf/timestamp.proto --out grpc-client/src --includeDir google-protos --includeDir protos
+compile-proto-file --proto ledger/queryresult/kv_query_result.proto --out protos-hs --includeDir google-protos --includeDir protos
+compile-proto-file --proto peer/chaincode_shim.proto --out protos-hs --includeDir google-protos --includeDir protos
+compile-proto-file --proto peer/chaincode_event.proto --out protos-hs --includeDir google-protos --includeDir protos
+compile-proto-file --proto peer/chaincode.proto --out protos-hs --includeDir google-protos --includeDir protos
+compile-proto-file --proto peer/proposal_response.proto --out protos-hs --includeDir google-protos --includeDir protos
+compile-proto-file --proto peer/proposal.proto --out protos-hs --includeDir google-protos --includeDir protos
+compile-proto-file --proto msp/msp_principal.proto --out protos-hs --includeDir google-protos --includeDir protos
+compile-proto-file --proto common/policies.proto --out protos-hs --includeDir google-protos --includeDir protos
+compile-proto-file --proto google/protobuf/timestamp.proto --out protos-hs --includeDir google-protos --includeDir protos
