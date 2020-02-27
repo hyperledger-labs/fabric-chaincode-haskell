c870ca5214b73e995c7db1c1115345b05808e5e3
Nick Waywood <nwaywood@au1.ibm.com>
2020-02-27 16:48:22 +1100

    Initial test framework.
    Signed-off-by: Nick Waywood <n.waywood@gmail.com>

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+import           Test.Tasty
+import           Test.Tasty.HUnit
+
+import           Stub
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [stubUnitTests]
+
+stubUnitTests :: TestTree
+stubUnitTests = testGroup "Stub Unit Tests" [getState]
+
+getTxId :: TestTree
+getState = testCase "getState" $ [1, 2, 3] `compare` [1, 2] @?= GT
+    -- where stub =
